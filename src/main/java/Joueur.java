41c58e146589b40e8a3953b756d7378767c227ca
Bamba NDIAYE <bamba.ndiaye-ext@amundi.com>
2016-12-12 19:05:43 +0100

    Test echoue incremente set

--- src/main/java/Joueur.java
+++ src/main/java/Joueur.java
@@ -2,4 +2,62 @@
  * Created by ndiaye on 12/12/2016.
  */
 public class Joueur {
+    protected int set;
+    protected int jeu;
+    protected int tieBreak;
+    protected boolean avantage;
+    protected String nom;
+
+
+    public Joueur(String nom) {
+        this.set = 0 ;
+        this.jeu = 0;
+        this.tieBreak = 0;
+        this.avantage = false;
+        this.nom = nom;
+    }
+    public int getSet() {
+        return set;
+    }
+
+    public void setSet(int set) {
+        this.set = set;
+    }
+
+    public int getJeu() {
+        return jeu;
+    }
+
+    public void setJeu(int jeu) {
+        this.jeu = jeu;
+    }
+
+    public int getTieBreak() {
+        return tieBreak;
+    }
+
+    public void setTieBreak(int tieBreak) {
+        this.tieBreak = tieBreak;
+    }
+
+    public boolean isAvantage() {
+        return avantage;
+    }
+
+    public void setAvantage(boolean avantage) {
+        this.avantage = avantage;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void incrementSet(){
+      
+    }
+
 }
--- src/test/java/JoueurTest.java
+++ src/test/java/JoueurTest.java
@@ -1,5 +1,18 @@
 /**
  * Created by ndiaye on 12/12/2016.
  */
-public class JoueurTest {
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JoueurTest   {
+
+    @Test
+    public void incrementSetTest(){
+        Joueur j=new Joueur("J1");
+        j.incrementSet();
+        Assert.assertTrue(j.getSet()==1);
+    }
+
+
 }
